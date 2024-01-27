e858687d17747eb0c31e13cdc8162d2cc9d3379a
MKampfrath <chelli@mailbox.org>
2024-01-27 15:18:03 +0100

    update interface score script call

--- scripts/pdb_rosetta_interface.sh
+++ scripts/pdb_rosetta_interface.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-echo "################ interface.sh before conda #######################"
 conda activate pyrose
 #cd /scratch/mutationexplorer/rasp-batch
-echo "######################### interface.sh ##################################"
-python /scratch/mutationexplorer/server/scripts/pdb_rosetta_interface.py  $1 $2 -i $3
+#echo "$(pwd)"
+#echo 'python /scratch/mutationexplorer-beta/server/scripts/pdb_rosetta_interface.py'  $1 $2 '-i' $3
+python /scratch/mutationexplorer-beta/server/scripts/pdb_rosetta_interface.py $1 $2 -i $3
 conda deactivate
