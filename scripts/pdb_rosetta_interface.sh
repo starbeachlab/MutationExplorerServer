b1e4ce7d5b2ed142c55d659fbed6bf83e3ee080f
renedominik <renedominik@yahoo.de>
2024-01-30 21:41:53 +0100

    changes due to merge

--- scripts/pdb_rosetta_interface.sh
+++ scripts/pdb_rosetta_interface.sh
@@ -4,5 +4,5 @@ conda activate pyrose
 #cd /scratch/mutationexplorer/rasp-batch
 #echo "$(pwd)"
 #echo 'python /scratch/mutationexplorer-beta/server/scripts/pdb_rosetta_interface.py'  $1 $2 '-i' $3
-python /scratch/mutationexplorer-beta/server/scripts/pdb_rosetta_interface.py $1 $2 -i $3
+python /scratch/mutationexplorer/server/scripts/pdb_rosetta_interface.py $1 $2 -i $3
 conda deactivate
